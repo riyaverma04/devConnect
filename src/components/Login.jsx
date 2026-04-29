eedee11652abefee563f1b2984762cdb3b17c570
riyaverma <techriya054@gmail.com>
2026-04-29 15:40:03 +0530

    update dependencies, add login functionality, and configure Tailwind CSS

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
 
 const Login = () => {
+  const [loginData, setLoginData]= useState({email:"riya@gmail.com", password: "riya054@Hey"})
+  // const [email, setEmail] = useState('riya@gmail.com')
+  // const [password, setPassword] = useState('riya054@Hey');
+
+const handleChange = (e)=>{
+
+  setLoginData((prev)=>({...prev, [e.target.name]: e.target.value}));
+  
+}
+
+  const handleLogin = async()=>{
+    try{
+      console.log(loginData)
+      const res = await axios.post('http://localhost:7777/login', loginData,{withCredentials: true});
+      console.log(res)
+
+    }catch(err){
+
+
+    }
+  }
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex items-center justify-center h-screen">
+      <div className="card bg-orange-500 text-white-content w-5/12 p-6">
+      <h1 className='text-center text-4xl font-bold'>Login {loginData.email}</h1>
+  <div className="card-body items-center text-center">
+
+    <label className="input validator">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+      <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+    </g>
+  </svg>
+  <input type="email" placeholder="mail@site.com" required name='email' value={loginData.email} onChange={(e) => handleChange(e)}  />
+</label>
+<div className="validator-hint hidden">Enter valid email address</div>
+
+
+    <label className="input validator">
+  <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+    <g
+      strokeLinejoin="round"
+      strokeLinecap="round"
+      strokeWidth="2.5"
+      fill="none"
+      stroke="currentColor"
+    >
+      <path
+        d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"
+      ></path>
+      <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+    </g>
+  </svg>
+  <input
+    type="password"
+    required
+    placeholder="Password"
+    minLength="8"
+    name='password'
+    value={loginData.password} onChange={(e) => handleChange(e)}
+   
+  />
+</label>
+<p className="validator-hint hidden">
+  Must be more than 8 characters, including
+  <br />At least one number <br />At least one lowercase letter <br />At least one uppercase letter
+</p>
+
+<button className="btn btn-outline btn-warning" onClick={handleLogin}>Login</button>
+    
+</div>
+    </div>
     </div>
   )
 }
